cdde4324051bb63b4846a3c8b1f6ae9a852edc80
juniorBM <juniorbev.mendonca@gmail.com>
2023-04-25 17:17:14 -0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LikeWidgetComponent } from './shared/components/like-widget/like-widget.component';
+import {LikeWidgetModule} from "./shared/components/like-widget/like-widget.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, LikeWidgetModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,25 +1,60 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { LikeWidgetComponent } from './like-widget.component';
+import {LikeWidgetComponent} from './like-widget.component';
+import {LikeWidgetModule} from "./like-widget.module";
 
-describe('LikeWidgetComponent', () => {
+describe(LikeWidgetComponent.name, () => {
   let component: LikeWidgetComponent;
   let fixture: ComponentFixture<LikeWidgetComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LikeWidgetComponent ]
+      imports: [LikeWidgetModule]
     })
-    .compileComponents();
+      .compileComponents();
+    fixture = TestBed.createComponent(LikeWidgetComponent);
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LikeWidgetComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
+    // const component = fixture.componentInstance;
     expect(component).toBeTruthy();
+  });
+
+  it('should auto generate ID during ngOnInit when (@Input id) is not assigned', () => {
+    // const component = fixture.componentInstance;
+    fixture.detectChanges();
+    expect(component.id).toBeTruthy();
+  });
+
+  it('should not auto generate ID during ngOnInit when (@Input id) is assigned', () => {
+    // const component = fixture.componentInstance;
+    const someId = 'someId';
+    component.id = 'someId';
+    fixture.detectChanges();
+
+    expect(component.id).toBe(someId);
+  })
+
+  // it(`#${LikeWidgetComponent.prototype.like.name} should trigger  when called`, done => {
+  //   fixture.detectChanges();
+  //
+  //   component.liked.subscribe(() => {
+  //     expect(true).toBeTrue();
+  //     done();
+  //   });
+  //   component.like();
+  // });
+
+  it(`#${LikeWidgetComponent.prototype.like.name} should trigger (@Output liked)  when called`, () => {
+    spyOn(component.liked, 'emit');
+    fixture.detectChanges();
+    component.like();
+    expect(component.liked.emit).toHaveBeenCalled();
   });
 });
